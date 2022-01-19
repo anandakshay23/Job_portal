225743f6c4e3b5c14905a665fcdddb5d60f2e2be
Akshay Anand <akshayanand@Akshays-MacBook-Air.local>
2022-01-19 21:01:31 +0530

    Connection Changed

--- include/config.php
+++ include/config.php
@@ -1,7 +1,7 @@
 <?php
 defined('server') ? null : define("server", "localhost");
 defined('user') ? null : define ("user", "root") ;
-defined('pass') ? null : define("pass","");
+defined('pass') ? null : define("pass","root");
 defined('database_name') ? null : define("database_name", "erisdb") ;
 
 $this_file = str_replace('\\', '/', __File__) ;
